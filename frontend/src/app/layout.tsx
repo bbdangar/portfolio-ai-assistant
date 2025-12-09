a218866bbe69761cee403f9545d9ad07bf4a23fc
Bhagirath Dangar <bhagirath@techlify.com>
2025-12-09 22:05:07 +0530

    font fix

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,20 +1,15 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Poppins } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const poppins = Poppins({
   subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  weight: ["300", "400", "500", "600"]
 });
 
 export const metadata: Metadata = {
   title: "Bhagirath Dangar - Full Stack & AI Engineer | Laravel • Angular • Vue • React | Android • React Native • Ionic",
-  description: "Bhagirath Dangar - Full Stack & AI Engineer | Laravel • Angular • Vue • React | Android • React Native • Ionic",
+  description: "Experienced Full Stack Developer with over 10 years of expertise in Laravel, Angular, and scalable application development. Currently expanding expertise into Machine Learning and Generative AI, specializing in Retrieval-Augmented Generation (RAG), LLM integration, and AI-driven automation. Combines deep backend and frontend engineering skills with modern AI capabilities to deliver intelligent, production-ready solutions for enterprise and startup environments.",
 };
 
 export default function RootLayout({
@@ -25,7 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${poppins.className} antialiased`}
       >
         {children}
       </body>
