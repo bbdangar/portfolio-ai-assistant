2a588c0b6cf6ff0ce70ead699aa63cfa9732fb95
Bhagirath Dangar <bhagirath@techlify.com>
2025-12-09 22:33:08 +0530

    analytics added

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next";
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -23,6 +24,7 @@ export default function RootLayout({
         className={`${poppins.className} antialiased`}
       >
         {children}
+        <Analytics />
       </body>
     </html>
   );
